f9fb3b2c434deeaef262fe8e12036eb60be83713
Mykhailo Hoshii <michanja13@gmail.com>
2025-07-05 19:00:00 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -7,12 +7,12 @@ def copy_file(command: str) -> None:
     source_file_name = parts[1]
     target_file_name = parts[2]
 
-    if source_file_name == target_file_name:
+    if source_file_name.lower() == target_file_name.lower():
         return
 
     try:
-        with open(source_file_name, "r", encoding="utf-8") as source_file, \
-                open(target_file_name, "w", encoding="utf-8") as target_file:
+        with open(source_file_name, "r") as source_file, \
+                open(target_file_name, "w") as target_file:
             target_file.write(source_file.read())
     except FileNotFoundError:
         return
